6c20962f822c0b9d46bef74e361b0d3909af192a
srluizz <133146564+srluizz@users.noreply.github.com>
2025-11-05 23:01:02 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,34 @@
+import streamlit as st 
+import requests 
+
+url = "https://dog.ceo/api/breeds/image/random"
+
+
+#Titulo da página
+st.markdown("<h1 style='text-align: center'>Gerador de cachorros</h1>", unsafe_allow_html=True)
+
+### Colunas ###
+col1, col2, col3 = st.columns(3)
+
+### Botão Novo Cachorro ###
+with col2:
+    if st.button("🐶 Novo Cachorro 🐶"):
+        st.rerun()
+    
+dados = requests.get(url).json()
+
+with col2:
+    st.image(dados['message'])
+    
+
+#Footer
+st.markdown(
+    """
+    <hr>
+    <p style='text-align: center; color: gray; font-size: 14px;'>
+        Desenvolvido por <b>Luiz Roberto</b> 🐾 | Dados da API: <a href='https://dog.ceo/dog-api/' target='_blank'>Dog CEO</a> | <a href="https://instagram.com/luizrobberto">Instagram </a>
+    </p>
+    """,
+    unsafe_allow_html=True
+)
+
